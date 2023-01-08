ed8e7dc51cc6c921a126e1e07a7455b2fb5fde94
honziktillu <till@spsmb.cz>
2023-01-08 23:51:52 +0100

    vybuch

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+
+/**
+ * TODO List
+ * 1. Setup react router
+ * 2. Single user page
+ * 3. Create repo
+ */
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
+};
+
+export default App;
